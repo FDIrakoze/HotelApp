5f8d2657cdca441afb7d023b0edc06cb4befd416
Irakoze Franco-davy <irakoze@b10p16.fil.univ-lille1.fr>
2019-11-04 09:48:39 +0100

    correct test-8

--- HotelDatabase/src/test/java/com/javaworld/hotels/model/HotelModelTest.java
+++ HotelDatabase/src/test/java/com/javaworld/hotels/model/HotelModelTest.java
@@ -40,7 +40,7 @@ public class HotelModelTest extends TestCase {
 	public void testFindHotelsByEmptyCity() {
 		HotelModel finder = new HotelModel();
 		List<Hotel> hotels = finder.findHotelsByCity("");
-		assertFalse(hotels.size() == 0);
+		assertTrue(hotels.size() == 0);
 	}
 
 	public void testFindHotelsByNullCity() {
